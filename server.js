961a3e2baec2c35e8228d33782492888fb013daa
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-13 07:06:15 +0200

    login checks

--- server.js
+++ server.js
@@ -4,22 +4,30 @@ import mongoose from 'mongoose';
 import { JobSource } from './models/JobSource.js';
 import cors from 'cors';
 
+const user = {
+    id: 1,
+    username: 'hans',
+    firstName: 'Hans',
+    lastName: 'Richter'
+};
+
 dotenv.config();
 
 const MONGODB_URI =
-	process.env.MONGODB_URI || 'mongodb://localhost/ejt-job-manager';
+    process.env.MONGODB_URI || 'mongodb://localhost/ejt-job-manager';
 
 mongoose.connect(MONGODB_URI, (err) => {
-	if (err) {
-		console.log({
-			error: "Cannot connect to MongoDB database.",
-			err: `"${err}"`
-		});
-	} 
+    if (err) {
+        console.log({
+            error: 'Cannot connect to MongoDB database.',
+            err: `"${err}"`
+        });
+    }
 });
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 3044;
 
 app.get('/', (req, res) => {
@@ -27,12 +35,20 @@ app.get('/', (req, res) => {
 });
 
 app.get('/job-sources', async (req, res) => {
-	const jobSources = await JobSource.find();
-	res.status(200).json(jobSources);
+    const jobSources = await JobSource.find();
+    res.status(200).json(jobSources);
 });
 
-app.post('/login', async (req, res) => {
-	res.status(200).json({"username": "hans"});
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username === 'hans' && password === '123') {
+        res.json({
+            user
+        });
+    } else {
+        res.sendStatus(500);
+    }
 });
 
 app.listen(port, () => {
